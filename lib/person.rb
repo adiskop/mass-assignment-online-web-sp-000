e0c72c73cfa6fd0f06acb177e7552693409c8b1e
Adi Skop <adiskop@gmail.com>
2020-07-25 10:00:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,7 +8,7 @@ attributes={:name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width}
 
-initialize (attributes)
+initialize (attributes=nil)
 
 
 
